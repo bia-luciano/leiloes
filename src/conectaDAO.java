0725b6cd3dc02f7cacccc06a442a031b16cdf169
bia-luciano <beatriz.desouza953@gmail.com>
2025-06-02 13:41:38 -0300

    Cadastro e lista de produtos, conexão com o SQL

--- src/conectaDAO.java
+++ src/conectaDAO.java
@@ -4,30 +4,18 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import javax.swing.JOptionPane;
 
-
-
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author Adm
- */
 public class conectaDAO {
     
     public Connection connectDB(){
-        Connection conn = null;
-        
-        try {
-        
-            conn = DriverManager.getConnection("jdbc:mysql://localhost/uc11?user=root&password=");
-            
-        } catch (SQLException erro){
-            JOptionPane.showMessageDialog(null, "Erro ConectaDAO" + erro.getMessage());
-        }
-        return conn;
+    Connection conn = null;
+    try {
+        Class.forName("com.mysql.cj.jdbc.Driver");  
+        conn = DriverManager.getConnection("jdbc:mysql://localhost/uc11?user=root&password=alessiA22!&useSSL=false");
+    } catch (ClassNotFoundException | SQLException erro){
+        JOptionPane.showMessageDialog(null, "Erro ConectaDAO: " + erro.getMessage());
     }
+    return conn;
+}
+
     
 }
